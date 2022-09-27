8b22714e72894f080dc4cc6a1d28668c60af6967
Jason Armstrong <jasnarmstrng@gmail.com>
2022-09-27 22:03:16 +0100

    Added solution thousandAndFirstPrime.py

---
+++ 7-10001st-prime/thousandAndFirstPrime.py
@@ -0,0 +1,33 @@
+# Question:
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+from math import floor, sqrt
+
+def isPrime(x):
+    # The range is based on the fact that a composite number must have a factor
+    # less than or equal to the square root of that number. Otherwise, the
+    # number is prime.
+    prime=True
+    if x>1:
+        for i in range(2,floor(sqrt(x)+1)):
+            if x%i==0:
+                prime = False
+                break
+    return prime
+
+def nthPrime(n):
+    num=1 # Number to check. Incremented in while loop
+    primeCounter=0 # Tracks the number of primes found
+    lastPrime=0 # The last prime found
+
+    while primeCounter<=n:
+        if isPrime(num):
+            primeCounter+=1
+            lastPrime=num
+        num+=1
+    return lastPrime
+
+if __name__=="__main__":
+    # print(nthPrime(6)) # -> 13
+    # print(nthPrime(100)) # -> 541
+    print(nthPrime(10001)) # -> 104743
