fa1fcc8cf0a7025ab72d385d381331ff81945f98
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-07 23:16:34 +0100

    Add solution largestProductInSeries.py

--- 8-largest-product-in-series/largestProductInSeries.py
+++ 8-largest-product-in-series/largestProductInSeries.py
@@ -1,30 +1,18 @@
 # Ques: Project Euler ques 8.
 # Given a positive number length n and positive number K find the maximum product of any contiguous subarray of size K.
 
-def largestProduct(K, number):
+import math
+
+def largestProduct(K=int,number=int):
     strNum=str(number)
-    maxProduct=1
-    windowProduct=1
-    windowStart=0
-    print("strNum: {}".format(strNum))
-    for windowEnd in range(len(strNum)):
-        windowProduct*=int(strNum[windowEnd])
-        print("windowProduct: {}".format(windowProduct))
+    window=[]
+    maxProduct=0
 
-        if windowEnd>=K-1:
-            maxProduct=max(maxProduct,windowProduct)
-            print("windowProduct, maxProduct: {}, {}".format(windowProduct, maxProduct))
-            outgoingVal=int(strNum[windowStart])
-            print("outVal: {}".format(outgoingVal))
-            # From here can't handle zero, need to update the algo
-            windowProduct/=outgoingVal
-            windowStart+=1
+    for i in range(len(strNum)):
+        window.append(int(strNum[i]))
 
-def main():
-    print("Test 1")
-    largestProduct(3, 731671)
-    print("-----------------------------------------------------------------------------")
-    print("Test 2")
-    largestProduct(3, 730671) # fails because there's a division by 0 here - windowProduct/=outgoingVal
+        if i>=K-1:
+            maxProduct=max(maxProduct,math.prod(window))
+            window.pop(0)
 
-main()
+    return maxProduct
