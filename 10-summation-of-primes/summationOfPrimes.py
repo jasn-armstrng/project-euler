8ece5fe7793bd6aabdc7d2a27ac9ea26d4c4db3f
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-10 23:07:21 +0100

    Add brute force solution summationOfPrimes.py

--- 10-summation-of-primes/summationOfPrimes.py
+++ 10-summation-of-primes/summationOfPrimes.py
@@ -1,3 +1,24 @@
 # Question: PE 10
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
+from math import floor, sqrt
+
+def isPrime(x):
+    # The range is based on the fact that a composite number must have a factor
+    # less than or equal to the square root of that number. Otherwise, the
+    # number is prime.
+    if x>1:
+        for i in range(2,floor(sqrt(x)+1)):
+            if x%i==0:
+                return False
+    else:
+        return False
+    return True
+
+
+def sumPrimesTo(n):
+    sum=2
+    for i in range (1,n,2):
+        if isPrime(i):
+            sum+=i
+    return sum
--- 10-summation-of-primes/test_summationOfPrimes.py
+++ 10-summation-of-primes/test_summationOfPrimes.py
@@ -6,5 +6,11 @@ class TestSummationOfPrimes(unittest.TestCase):
         result=summationOfPrimes.sumPrimesTo(10)
         self.assertEqual(result,17)
 
+        result=summationOfPrimes.sumPrimesTo(20)
+        self.assertEqual(result,77)
+
+        result=summationOfPrimes.sumPrimesTo(2000000)
+        self.assertEqual(result,142913828922)
+
 if __name__=="__main__":
     unittest.main()
