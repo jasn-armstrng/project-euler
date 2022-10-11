d34856d57bd65930e3731015fbe758ea9f01bb56
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-11 23:22:59 +0100

    Apply Google style guide for Python

--- 1-multiples-of-3-or-5/mult3Or5.py
+++ 1-multiples-of-3-or-5/mult3Or5.py
@@ -2,12 +2,12 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-# -> Find the sum of all the multiples of x or y below K.
 
-def sumOfMultiples(x:int, y:int, K:int)->int:
-    sum = 0
-    start=min(x,y)
-    for i in range(start, K):
-        if i%x==0 or i%y==0:
-            sum+=i
-    return sum
+def sum_of_multiples(x: int, y: int, K: int) -> int:
+  sum = 0
+
+  for i in range(min(x, y), K):
+    if i % x == 0 or i % y == 0:
+      sum += i
+
+  return sum
