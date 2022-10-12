5b0a0d7e0e4dcdcda7de598345b48968e97f22dc
Jason Armstrong <jasnarmstrng@gmail.com>
2022-10-12 13:33:21 +0100

    Minor style update

--- 1-multiples-of-3-or-5/test_sum_of_multiples.py
+++ 1-multiples-of-3-or-5/test_sum_of_multiples.py
@@ -1,22 +1,23 @@
 import unittest
-import sum_of_multiples as som
+from sum_of_multiples import sum_multiples
 
 class TestSumOFMultiples(unittest.TestCase):
-  def test_sum_of_multiples(self):
-    '''Test cases for function sum_of_multiples'''
-    result = som.sum_of_multiples(3, 5, 10)
-    self.assertEqual(result, 23)
 
-    result = som.sum_of_multiples(3, 5, 100)
-    self.assertEqual(result, 2318)
+    def test_sum_multiples(self):
+        '''Test cases for function sum_of_multiples.'''
+        result = sum_multiples(3, 5, 10)
+        self.assertEqual(result, 23)
 
-    result = som.sum_of_multiples(3, 5, 1000)
-    self.assertEqual(result, 233168)
+        result = sum_multiples(3, 5, 100)
+        self.assertEqual(result, 2318)
 
-    result = som.sum_of_multiples(3, 5, 10000)
-    self.assertEqual(result, 23331668)
+        result = sum_multiples(3, 5, 1000)
+        self.assertEqual(result, 233168)
 
-    # Interesting pattern forming from multiples of 3, 5 and powers of 10.
+        result = sum_multiples(3, 5, 10**4)
+        self.assertEqual(result, 23331668)
+        # Interesting pattern forming from multiples of 3, 5 and powers of 10.
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
